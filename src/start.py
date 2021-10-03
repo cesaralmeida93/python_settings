3c82ff9f1cee19f130b0d986f21a5791df43fd87
cesar <ocesaralmeida@gmail.com>
2021-10-03 07:10:21 -0300

    config: Setting project configs

---
+++ src/start.py
@@ -0,0 +1,12 @@
+def soma(num_1: int, num_2: int) -> int:
+    """Somando dois numeros
+    :param - num_1: primeiro numero da soma
+           - num_2: segundo numero da soma
+    :return - Soma entre dois numeros
+    """
+
+    soma_dois_numeros = num_1 + num_2
+    return soma_dois_numeros
+
+
+SOMA_DOIS_NUMEROS = soma(1, 2)
